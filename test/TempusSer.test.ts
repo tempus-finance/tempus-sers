f3e69c8d7c5276dbfe26c50afdfac94248fc5337
Alex Beregszaszi <alex@rtfs.hu>
2021-10-15 23:19:49 +0100

    Add test case to claim all tokens until MAX_SUPPLY

--- test/TempusSer.test.ts
+++ test/TempusSer.test.ts
@@ -243,5 +243,20 @@ describe("Tempus Sers", async () => {
       expect(await token.claimedTickets(ticketId)).to.equal(true);
       expect(await token.originalMinter(tokenId)).to.equal(user.address);
     });
+    it("Should work with redeeming all tickets", async () => {
+      await token.setSeed();
+      const maxSupply = await token.MAX_SUPPLY();
+      for (let ticketId = 0; ticketId < maxSupply; ticketId++) {
+        const tokenId = await token.ticketToTokenId(BigNumber.from(ticketId));
+        const tokenURI = (await token.baseTokenURI()) + tokenId + ".json";
+        expect(await token.claimedTickets(ticketId)).to.equal(false);
+        // Transfer(0, to, tokenId);
+        expect(await redeemTicket(owner, user.address, ticketId))
+          .to.emit(token, "Transfer").withArgs("0x0000000000000000000000000000000000000000", user.address, tokenId)
+          .to.emit(token, "PermanentURI").withArgs(tokenURI, tokenId);
+        expect(await token.claimedTickets(ticketId)).to.equal(true);
+        expect(await token.originalMinter(tokenId)).to.equal(user.address);
+      }
+    }).timeout(300000);
   });
 });
